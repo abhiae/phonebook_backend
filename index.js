d148709f722d7758b8327d4e8623c1b5518c7945
Abhinav Jaishi <56696573+abhiae@users.noreply.github.com>
2024-11-01 09:55:17 +0545

    solved the phonebook backend from  3.15 to 3.18

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const cors = require("cors");
 require("dotenv").config();
 const Contact = require("./models/contact");
 
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(cors());
-app.use(express.static("dist"));
+
 //creating token to log post request data
 morgan.token("post-data", (req, res) => {
   return req.method === "POST" ? JSON.stringify(req.body) : null;
@@ -24,28 +25,31 @@ app.get("/api/persons", (request, response) => {
     response.json(contacts);
   });
 });
-app.get("/api/persons/:id", (request, response) => {
-  Contact.findById(request.params.id).then((contact) => {
-    response.json(contact);
-  });
+
+app.get("/api/persons/:id", (request, response, next) => {
+  Contact.findById(request.params.id)
+    .then((contact) => {
+      if (contact) {
+        response.json(contact);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/info", (request, response) => {
-  const date = new Date();
-  response.send(
-    `<p>Phonebook has info for ${Contact.length} people</p> <p>${date}</p>` // this way of length doesn't work need to check on internet
-  );
+  Contact.countDocuments({})
+    .then((count) => {
+      const date = new Date();
+      response.send(
+        `<p>Phonebook has info for ${count} people</p> <p>${date}</p>`
+      );
+    })
+    .catch((error) => next(error));
 });
 
-const nameExists = (searchname) => {
-  Contact.findOne({ name: searchname }).then((person) => {
-    console.log(person.name);
-    console.log("search name:", searchname);
-    const boolValue = person.name === searchname;
-    console.log("bool:", boolValue);
-    return boolValue;
-  });
-};
+// adding contact to the db
 app.post("/api/persons", (request, response) => {
   const body = request.body;
   if (!body.name || !body.number) {
@@ -53,11 +57,7 @@ app.post("/api/persons", (request, response) => {
       .status(400)
       .json({ error: "either name or number missing" });
   }
-  if (nameExists(body.name)) {
-    return response
-      .status(400)
-      .json({ error: "this name already exists, name must be unique" });
-  }
+
   const person = new Contact({
     name: body.name,
     number: body.number,
@@ -68,11 +68,39 @@ app.post("/api/persons", (request, response) => {
   });
 });
 
-app.delete("/api/persons/:id", (request, response) => {
-  const id = String(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-  response.status(204).end();
+// adding support for put when the new number added for same name
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Contact.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      console.log("person in put:", person);
+      console.log("updatedPerson:", updatedPerson);
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
 });
+
+app.delete("/api/persons/:id", (request, response, next) => {
+  Contact.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
+});
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
